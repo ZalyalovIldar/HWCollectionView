fb35336703746e2c3f1b2bdf337652f3b92c74f9
Ильдар Залялов <Zalyalov.Ild@gmail.com>
2016-11-03 23:42:09 +0300

    Initial Commit

---
+++ HWCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWCollectionView
+//
+//  Created by Ildar Zalyalov on 03.11.16.
+//  Copyright © 2016 com.itis.iosLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWCollectionView
+//
+//  Created by Ildar Zalyalov on 03.11.16.
+//  Copyright © 2016 com.itis.iosLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
