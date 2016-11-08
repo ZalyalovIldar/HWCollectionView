24f7a14b46c37ae6a61e8f5f80aa8d71191bb15e
Rustamnurg <Rustik6030@mail.ru>
2016-11-09 01:33:18 +0300

    hw

---
+++ HWCollectionView/CollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.h
+//  HWCollectionView
+//
+//  Created by Rustam N on 08.11.16.
+//  Copyright © 2016 com.itis.iosLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewCell;
+
+
+@end
